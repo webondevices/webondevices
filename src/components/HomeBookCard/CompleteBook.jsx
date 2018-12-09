1a67e3169c8e75b58728cb20f131652ee883bd8f
Mate Marschalko <webondevices@gmail.com>
2018-12-09 21:33:06 +0000

    Updating source files

--- src/components/HomeBookCard/CompleteBook.jsx
+++ src/components/HomeBookCard/CompleteBook.jsx
@@ -2,26 +2,29 @@ import React, { Component } from "react";
 import "./CompleteBook.scss";
 
 class CompleteBook extends Component {
-    render () {
-        return(
+  render() {
+    return (
+      <div>
+        <div className="book-content">
           <div>
-            <div className="book-content">
-              <div>
-                <h2>Physical Book</h2>
-                <p className="highlighted-text">Learn all about programming electronics JavaScript and Node.js, all from the very basics.</p>
-                <p>Build a smart talking plant, a motion sensing alarms, learn data logging, send SMS notifactions and many more!</p>
-              </div>
-              <div>
-                <img className="index-card-book" alt="Introduction to JavaScript Electronics" src="/js-electronics.png" />
-              </div>
-            </div>
-            <button className="cta-button amazon-cta action" onClick={() => {window.open('https://www.amazon.com/JavaScript-Electronics-Hardware-Prototyping-Node-js/dp/1548255521/ref=sr_1_1')}} type="button">Amazon.com</button>
-            <button className="cta-button amazon-cta action" onClick={() => {window.open('https://www.amazon.co.uk/JavaScript-Electronics-Hardware-Prototyping-Node-js/dp/1548255521/ref=sr_1_1')}} type="button">Amazon.co.uk</button>
-            <button className="cta-button amazon-cta action" onClick={() => {window.open('https://www.amazon.de/JavaScript-Electronics-Hardware-Prototyping-Node-js/dp/1548255521/ref=sr_1_1')}} type="button">Amazon.de</button>
-            <button className="cta-button amazon-cta action" onClick={() => {window.open('https://www.amazon.fr/JavaScript-Electronics-Hardware-Prototyping-Node-js/dp/1548255521/ref=sr_1_1')}} type="button">Amazon.fr</button>
+            <h2>Complete book</h2>
+            <p className="highlighted-text">Learn all about programming electronics JavaScript and Node.js, all from the very basics.</p>
+            <p>Build a smart talking plant, a motion sensing alarms, learn data logging, send SMS notifactions and many more!</p>
           </div>
-        );
-    }
+          <div>
+            <img className="index-card-book" alt="Introduction to JavaScript Electronics" src="/js-electronics.png" />
+          </div>
+        </div>
+        <p className="intermediate-copy"><strong>Get the print edition from Amazon for only $12.95 - £12.95:</strong></p>
+        <div>
+          <button className="cta-button amazon-cta action" onClick={() => { window.open('https://www.amazon.com/dp/1790464919') }} type="button">Amazon.com</button>
+          <button className="cta-button amazon-cta action" onClick={() => { window.open('https://www.amazon.co.uk/dp/1790464919') }} type="button">Amazon.co.uk</button>
+          <button className="cta-button amazon-cta action" onClick={() => { window.open('https://www.amazon.de/dp/1790464919') }} type="button">Amazon.de</button>
+          <button className="cta-button amazon-cta action" onClick={() => { window.open('https://www.amazon.fr/dp/1790464919') }} type="button">Amazon.fr</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CompleteBook;
--- src/components/HomeBookCard/FreeBook.jsx
+++ src/components/HomeBookCard/FreeBook.jsx
@@ -3,12 +3,12 @@ import "./FreeBook.scss";
 
 class FreeBook extends Component {
 
-  static isValidEmail (email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  static isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
 
-  constructor () {
+  constructor() {
     super();
     this.state = {
       email: '',
@@ -24,7 +24,7 @@ class FreeBook extends Component {
     this.submit = this.submit.bind(this);
   }
 
-  handleInput (event) {
+  handleInput(event) {
     this.setState({
       email: event.target.value,
       valid: FreeBook.isValidEmail(event.target.value),
@@ -32,7 +32,7 @@ class FreeBook extends Component {
     });
   }
 
-  submit (event) {
+  submit(event) {
     event.preventDefault();
 
     if (this.state.valid) {
@@ -40,49 +40,49 @@ class FreeBook extends Component {
       request.open('GET', `${this.apiEndpoint}?email=${this.state.email}`, true);
 
       request.onload = () => {
-        
+
         // Successfully submitted
         if (request.status === 200) {
-          this.setState({submitted: true});
-        
-        // Server error
-        } else {  
-          this.setState({pending: false, submitted: false, error: true});
+          this.setState({ submitted: true });
+
+          // Server error
+        } else {
+          this.setState({ pending: false, submitted: false, error: true });
         }
       };
 
       request.onerror = () => {
-        this.setState({pending: false, submitted: false, error: true});
+        this.setState({ pending: false, submitted: false, error: true });
       };
 
       request.send();
-      this.setState({pending: true});
-      
+      this.setState({ pending: true });
+
     }
   }
 
-  render () {
-      return(
-        <div>
-          <div className="book-content">
-            <div>
-              <h2>Free Ebook</h2>
-              <p className="highlighted-text">Get started with hardware prototyping and learn the basiscs in a few hours.</p>
-              <p>This ebook will introduce you to JavaScript Arduino Electronics: blink an LED light and read simple sensors!</p>
-            </div>
-            <div>
-              <img className="index-card-book" alt="Introduction to JavaScript Electronics" src="/intro-to-js-electronics.png" />
-            </div>
+  render() {
+    return (
+      <div>
+        <div className="book-content">
+          <div>
+            <h2>Introduction</h2>
+            <p className="highlighted-text">Get started with JavaScript Electronics and learn the basiscs in just a few hours!</p>
+            <p>Learn working with the Arduino UNO, building simple circuits, controlling an LED light and measuring light and temperature with JavaScript and Node.js!</p>
           </div>
-          <form className={`email-form${this.state.submitted ? ' submitted' : ''}${this.state.pending ? ' pending' : ''}${this.state.error ? ' error' : ''}${this.state.valid ? ' valid' : ' invalid'}`}>
-            <input type="email" placeholder="Email address" onChange={this.handleInput} />
-            <p className="pending-message">Submitting...</p>
-            <p className="error-message">Please try again!</p>
-            <button className="cta-button submit-email action" onClick={this.submit} type="submit">Send me the PDF</button>
-          </form>
-          <p className="thank-you-message">Check your emails!</p>
+          <div>
+            <img className="index-card-book" alt="Introduction to JavaScript Electronics" src="/intro-to-js-electronics.png" />
+          </div>
         </div>
-      );
+        <p className="intermediate-copy"><strong>Get the print edition from Amazon for only<br />$4.95 - £4.95:</strong></p>
+        <div>
+          <button className="cta-button amazon-cta action" onClick={() => { window.open('https://www.amazon.com/dp/1790516900') }} type="button">Amazon.com</button>
+          <button className="cta-button amazon-cta action" onClick={() => { window.open('https://www.amazon.co.uk/dp/1790516900') }} type="button">Amazon.co.uk</button>
+          <button className="cta-button amazon-cta action" onClick={() => { window.open('https://www.amazon.de/dp/1790516900') }} type="button">Amazon.de</button>
+          <button className="cta-button amazon-cta action" onClick={() => { window.open('https://www.amazon.fr/dp/1790516900') }} type="button">Amazon.fr</button>
+        </div>
+      </div>
+    );
   }
 }
 
